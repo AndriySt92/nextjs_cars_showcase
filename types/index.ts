f99dd9024403da436ae941ff5d2fae7a01ec8854
AndriySt92 <andriyua-1992@mail.ru>
2024-03-19 12:24:45 +0200

    Added SearchMenufacturer component

--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,23 @@ export interface CustomButtonProps {
     handleClick?: MouseEventHandler<HTMLButtonElement>;
     btnType?: "button" | "submit";
 }
+
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+}
+
+export interface SearchManufacturerProps {
+    menufacturer: string;
+    setMenufacturer: (menufacturer: string) => void;
+}
+
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+}
