bb807927786f0cd86fdd285ab6c7019bc31bfb53
AndriySt92 <andriyua-1992@mail.ru>
2024-03-19 15:54:00 +0200

    Added CarCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,49 @@
 import Image from 'next/image'
-import { Hero } from '@/components'
+import { fuels, yearsOfProduction } from "@/contstans";
+import { CarCard, CustomFilter, Hero, SearchBar } from '@/components'
+import { fetchCars } from '@/utils';
+import { CarProps } from '@/types';
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || !allCars.length;
+
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
+          </div>
+        </div>
+
+        {!isDataEmpty ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {allCars?.map((car: CarProps, index: number) => (
+                <CarCard car={car} key={index} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home_error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+            <p>{allCars?.length}</p>
+          </div>
+        )}
+
+      </div>
     </main>
   )
 }
--- types/index.ts
+++ types/index.ts
@@ -1,28 +1,46 @@
-import { MouseEventHandler } from "react";
+import { MouseEventHandler } from 'react'
 
 export interface CustomButtonProps {
-    title: string;
-    containerStyles?: string;
-    handleClick?: MouseEventHandler<HTMLButtonElement>;
-    btnType?: "button" | "submit";
+  title: string
+  containerStyles?: string
+  handleClick?: MouseEventHandler<HTMLButtonElement>
+  btnType?: 'button' | 'submit'
+  textStyles?: string
+  rightIcon?: string
+  isDisabled?: boolean
 }
 
 export interface CustomFilterProps {
-    title: string;
-    options: OptionProps[];
+  title: string
+  options: OptionProps[]
 }
 
 export interface SearchManufacturerProps {
-    menufacturer: string;
-    setMenufacturer: (menufacturer: string) => void;
+  menufacturer: string
+  setMenufacturer: (menufacturer: string) => void
 }
 
 export interface OptionProps {
-    title: string;
-    value: string;
+  title: string
+  value: string
 }
 
 export interface CustomFilterProps {
-    title: string;
-    options: OptionProps[];
+  title: string
+  options: OptionProps[]
+}
+
+export interface CarProps {
+  city_mpg: number
+  class: string
+  combination_mpg: number
+  cylinders: number
+  displacement: number
+  drive: string
+  fuel_type: string
+  highway_mpg: number
+  make: string
+  model: string
+  transmission: string
+  year: number
 }
